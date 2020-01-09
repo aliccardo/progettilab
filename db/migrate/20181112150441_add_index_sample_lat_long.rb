a750b4976b570d08f0cf5307c288861ab0eded12
Antonio Liccardo <antonio.liccardo@citelgroup.it>
2020-01-09 12:47:24 +0100

    Aggiunto un controllo per eliminare l'indice se già esiste al momento della esecuzione della migrazione.

--- db/migrate/20181112150441_add_index_sample_lat_long.rb
+++ db/migrate/20181112150441_add_index_sample_lat_long.rb
@@ -1,3 +1,4 @@
 class AddIndexSampleLatLong < ActiveRecord::Migration[5.2]
+  remove_index :samples, [:latitude, :longitude] if index_exists?(:samples, [:latitude, :longitude])
   add_index :samples, [:latitude, :longitude]
 end
