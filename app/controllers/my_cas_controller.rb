38970a641b084c1257c6d1854608699384e408da
Antonio Liccardo <antonio.liccardo@citelgroup.it>
2020-02-13 13:22:12 +0100

    Rimosso il codice relativo all'autenticazione CAS.

--- app/controllers/my_cas_controller.rb
+++
@@ -1,7 +0,0 @@
-####################################################################
-# Non necessario con per l'autenticazioen LDAP.
-# class MyCasController < Devise::CasSessionsController
-#   Rails <= 3 skip_before_filter :redirect_to_sign_in, only: [:new, :single_sign_out]
-#   skip_before_action :redirect_to_sign_in, only: [:new, :single_sign_out]
-# end
-####################################################################
