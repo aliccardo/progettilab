183bef1f91aa62c3612e762401bca45091462d48
Antonio Liccardo <antonio.liccardo@citelgroup.it>
2020-06-04 17:55:56 +0200

    Soluzione del punto 1: il campo Minima Concentrazione Rivelabile (MCR) adesso può essere riempito senza i valori per la misura e la sua incertezza.

--- app/models/analisy_result.rb
+++ app/models/analisy_result.rb
@@ -12,19 +12,19 @@ class AnalisyResult < ApplicationRecord
   has_one :sample, :through => :analisy
   has_one :job, :through => :sample
   has_one :analisy_type, :through => :analisy, source: :type
-    
+
   has_many :logs, as: :loggable
   accepts_nested_attributes_for :logs, :reject_if => :all_blank, :allow_destroy => true
 
   accepts_nested_attributes_for :analisy, :reject_if => :all_blank, :allow_destroy => true
-  
+
   attr_accessor :author, :device, :import
 
-  validates :full_result, presence: true, if: Proc.new { |result| result.absence_analysis.blank? }
-  validates :full_result_with_nuclide, presence: true, if: Proc.new { |result| result.absence_analysis.blank? }
- 
-  validates_presence_of :result_unit_id, message: "Unità di misura mancante!", on: :update, if: Proc.new { |result| result.absence_analysis.blank? }
-  validates_presence_of :indecision_unit_id, message: "Unità di misura della incertezza mancante!", on: :update, if: Proc.new { |result| result.absence_analysis.blank? }
+  validates :full_result, presence: true, if: Proc.new { |result| result.absence_analysis.blank? || result.mcr.blank?}
+  validates :full_result_with_nuclide, presence: true, if: Proc.new { |result| result.absence_analysis.blank? || result.mcr.blank?}
+
+  validates_presence_of :result_unit_id, message: "Unità di misura mancante!", on: :update, if: Proc.new { |result| result.absence_analysis.blank?}
+  validates_presence_of :indecision_unit_id, message: "Unità di misura della incertezza mancante!", on: :update, if: Proc.new { |result| result.absence_analysis.blank?}
 
   before_validation :prerequisite
   before_save :create_log
@@ -53,13 +53,22 @@ class AnalisyResult < ApplicationRecord
   end
 
   def set_results
-    self.full_result = case symbol
-      when "±"
-        "#{result} #{result_unit.html unless result_unit_id.blank? } #{symbol} #{indecision} #{indecision_unit.html unless result_unit_id.blank?}"
-      when "<"
-        "#{symbol} #{result} #{result_unit.html unless result_unit_id.blank?}"
-      else
-        "n.d."
+    mcr_text = (mcr.blank?)? '':  "; MCR=#{mcr} #{result_unit.html unless result_unit_id.blank?}"
+    Rails.logger.debug "mcr è: #{mcr_text}."
+    Rails.logger.debug "result è: #{result}."
+    self.full_result = unless result.blank?
+      Rails.logger.debug "symbol è: #{symbol}."
+      case symbol
+        when "±"
+          "#{result} #{result_unit.html unless result_unit_id.blank?} #{symbol} #{indecision} #{indecision_unit.html unless result_unit_id.blank?}#{mcr_text unless mcr_text.blank? }"
+        when "<"
+          "#{symbol} #{result} #{result_unit.html unless result_unit_id.blank?}#{mcr_text unless mcr_text.blank? }"
+        else
+          (mcr_text.blank?)? 'n.d.': mcr_text[2..mcr_text.size]
+      end
+    else
+      Rails.logger.debug "ritorno : #{mcr_text[2..mcr_text.size]}."
+      mcr_text[2..mcr_text.size]
     end
   end
 
@@ -103,23 +112,27 @@ class AnalisyResult < ApplicationRecord
     if new_record? && import.blank?
       self.full_result = 'n.d.'
     else
+      Rails.logger.debug "absence_analysis è presente: #{absence_analysis.present?}."
       if absence_analysis.present?
         self.result = ''
         self.result_unit_id = :null
         self.indecision = ''
         self.indecision_unit_id = :null
+        self.mcr = ''
         self.full_result = ''
         self.absent = true
         self.full_result = absence_analysis
       else
-        errors.add :result, 'campo obbligatorio' if result.blank?
-        errors.add :result_unit_id, 'campo obbligatorio' if result_unit_id.blank?
-        errors.add :indecision, 'campo obbligatorio' if symbol == '±' && indecision.blank?
-        errors.add :indecision_unit_id, 'campo obbligatorio' if symbol == '±' && indecision_unit_id.blank?
+        errors.add :result, 'campo obbligatorio, se MCR è vuoto' if result.blank? && mcr.blank?
+        errors.add :result_unit_id, 'campo obbligatorio, se MCR è vuoto' if result_unit_id.blank? && mcr.blank?
+        errors.add :indecision, 'campo obbligatorio, se MCR è vuoto' if symbol == '±' && indecision.blank? && mcr.blank?
+        errors.add :indecision_unit_id, 'campo obbligatorio, se MCR è vuoto' if symbol == '±' && indecision_unit_id.blank? && mcr.blank?
         if errors.blank?
           self.absent = false
         end
+        Rails.logger.debug "imposto i risultati chiamando set_results."
         set_results
+        Rails.logger.debug "self.full_result è: #{self.full_result}."
       end
     end
     for_report
