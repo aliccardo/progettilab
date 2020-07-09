0e9f30323ad2c400546d8f6af1d052d7f33785e2
Antonio Liccardo <antonio.liccardo@citelgroup.it>
2020-07-09 16:30:40 +0200

    Nella stampa del rapporto singolo di analisi relativo alla tipologia di analisi "Concentrazione di attività di radon in aria (SSNTD’s)" (id == 1), la data di riferimento non è più stampata.

--- app/reports/pdfs/template1/single.rb
+++ app/reports/pdfs/template1/single.rb
@@ -93,10 +93,12 @@ module Single
 		stroke_rectangle [ rect_left, cursor+5 ], rect_width, 20
 		text_box "#{@report.analisy.performed_on}", :size => text_size, :at => [field_left, cursor]
 
-		move_down(25)
-		text_box I18n.t('reference_at', scope: 'reports.fields', default: 'Reference at').upcase, :size => label_size, :at => [left, cursor]
-		stroke_rectangle [ rect_left, cursor+5 ], rect_width, 20
-		text_box "#{@report.analisy.reference_on}", :size => text_size, :at => [field_left, cursor]
+		unless @report.analisy.type.id == 1 # analisy_type.title == 'Concentrazione di attività di radon in aria (SSNTD\'s)'
+			move_down(25)
+			text_box I18n.t('reference_at', scope: 'reports.fields', default: 'Reference at').upcase, :size => label_size, :at => [left, cursor]
+			stroke_rectangle [ rect_left, cursor+5 ], rect_width, 20
+			text_box "#{@report.analisy.reference_on}", :size => text_size, :at => [field_left, cursor]
+		end
 
 		move_down(25)
 		text_box I18n.t('analisy_type', scope: 'reports.fields', default: 'Analisy type').upcase, :size => label_size, :at => [left, cursor]
